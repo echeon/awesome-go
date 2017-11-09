dcb0a726cd7d1abc9d73323075f22eb8389b8012
%= GitConfig.user_name % <%= GitConfig.user_email %>
2017-11-08 23:11:38 -0500

    Update and add Procfile

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	log.Printf("Listening on %s...\n", addr)
+
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
+}
+
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
